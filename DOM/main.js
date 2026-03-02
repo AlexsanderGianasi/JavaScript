9b93f841e7b620b00a462d9665cfd06c432f9319
Alexsander Gianasi <206681800+AlexsanderGianasi@users.noreply.github.com>
2026-03-02 20:22:22 -0300

    .

---
+++ DOM/main.js
@@ -0,0 +1,4 @@
+function clicar() {
+    var he = document.getElementById("hero");
+    he.innerText = "Com o DOM, os desenvolvedores podem acessar e manipular o conteúdo, a estrutura e o estilo de um documento de forma dinâmica usando linguagens de programação como JavaScript. Isso permite criar páginas web interativas e dinâmicas, onde os elementos podem ser adicionados, removidos ou modificados em tempo real.";
+}
